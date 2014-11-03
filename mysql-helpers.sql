343c112001be057da013f96020a1a1d72a88c88e
glenwatson <soccer152@comcast.net>
2014-11-03 17:24:13 -0600

    Adds ORDER BY for quicker eye-scanning

--- mysql-helpers.sql
+++ mysql-helpers.sql
@@ -5,8 +5,9 @@ DROP PROCEDURE IF EXISTS me.tf;
 DELIMITER //
 CREATE PROCEDURE me.tf (table_part VARCHAR(512)) BEGIN
     SELECT table_schema, table_name 
-	    FROM information_schema.tables 
-        WHERE table_name LIKE CONCAT('%', table_part, '%');
+        FROM information_schema.tables 
+        WHERE table_name LIKE CONCAT('%', table_part, '%')
+        ORDER BY table_name, table_schema;
 END//
 DELIMITER ;
 
@@ -16,6 +17,7 @@ DELIMITER //
 CREATE PROCEDURE me.cf (column_part VARCHAR(512)) BEGIN
     SELECT table_schema, table_name, column_name, data_type, ordinal_position, is_nullable, column_default 
         FROM information_schema.columns 
-        WHERE column_name LIKE CONCAT('%', column_part, '%');
+        WHERE column_name LIKE CONCAT('%', column_part, '%')
+        ORDER BY column_name, table_name, table_schema;
 END//
 DELIMITER ;
