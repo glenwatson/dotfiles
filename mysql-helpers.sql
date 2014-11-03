294837a1127a01bf1f6d39820deff06ac8102940
glenwatson <soccer152@comcast.net>
2014-11-03 17:19:41 -0600

    Adds column find (me.cf) proc

--- mysql-helpers.sql
+++ mysql-helpers.sql
@@ -1,9 +1,21 @@
 CREATE SCHEMA IF NOT EXISTS me;
 
--- tableFind
+-- table find
 DROP PROCEDURE IF EXISTS me.tf;
 DELIMITER //
 CREATE PROCEDURE me.tf (table_part VARCHAR(512)) BEGIN
-	SELECT table_schema, table_name FROM information_schema.tables WHERE table_name LIKE CONCAT('%', table_part, '%');
+    SELECT table_schema, table_name 
+	    FROM information_schema.tables 
+        WHERE table_name LIKE CONCAT('%', table_part, '%');
+END//
+DELIMITER ;
+
+-- column find
+DROP PROCEDURE IF EXISTS me.cf;
+DELIMITER //
+CREATE PROCEDURE me.cf (column_part VARCHAR(512)) BEGIN
+    SELECT table_schema, table_name, column_name, data_type, ordinal_position, is_nullable, column_default 
+        FROM information_schema.columns 
+        WHERE column_name LIKE CONCAT('%', column_part, '%');
 END//
 DELIMITER ;
